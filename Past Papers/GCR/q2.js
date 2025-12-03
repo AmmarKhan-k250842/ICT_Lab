4a9a07fae184d51a58a3d9403a0680cb248f7330
AmmarKhan-k250842 <k250842@nu.edu.pk>
2025-12-03 20:29:34 +0500

    Add files via upload

---
+++ Past Papers/GCR/q2.js
@@ -0,0 +1,17 @@
+const rbtn = document.getElementById("red")
+rbtn.addEventListener("click", function change()
+{
+    rbtn.style.backgroundColor = "white";
+})
+
+const gbtn = document.getElementById("green")
+gbtn.addEventListener("click", function change()
+{
+    gbtn.style.backgroundColor = "white";
+})
+
+const ybtn = document.getElementById("yellow")
+ybtn.addEventListener("click", function change()
+{
+    ybtn.style.backgroundColor = "white";
+})
